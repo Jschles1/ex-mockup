c1c86f770525c473d41460506c7aeaa3b741d908
Jschles1 <jschles1@ramapo.edu>
2018-02-25 18:41:21 -0500

    Added content to Intro

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -9,20 +9,41 @@
     <div class="row">
       <div class="col-md-4">
         <article class="post">
-          <figure></figure>
-          <figcaption></figcaption>
+          <figure>
+            <a href="#">
+              <img src="https://www.exodus.io/img/exodus-portfolio-2017-08-thum@2x.jpg" alt="Exodus Porfolio">
+            </a>
+          </figure>
+          <figcaption>
+            <h6>PORTFOLIO</h6>
+            <p>Live charts turn your diversified portfolio into a bottom line.</p>
+          </figcaption>
         </article>
       </div>
       <div class="col-md-4">
         <article class="post">
-          <figure></figure>
-          <figcaption></figcaption>
+          <figure>
+            <a href="#">
+              <img src="https://www.exodus.io/img/exodus-exchange-aurora-ss-thum@2x.jpg" alt="Exodus Exchange">
+            </a>
+          </figure>
+          <figcaption>
+            <h6>EXCHANGE</h6>
+            <p>A seamless, one click, ShapeShift experience.</p>
+          </figcaption>
         </article>
       </div>
       <div class="col-md-4">
         <article class="post">
-          <figure></figure>
-          <figcaption></figcaption>
+          <figure>
+            <a href="#">
+              <img src="https://www.exodus.io/img/exodus-wallet-btc-thum@2x.jpg" alt="Exodus Wallet">
+            </a>
+          </figure>
+          <figcaption>
+            <h6>MULTI-ASSET WALLET</h6>
+            <p>Bitcoin, Ethereum, Litecoin, Dash, EOS and more.</p>
+          </figcaption>
         </article>
       </div>
     </div>
@@ -37,12 +58,31 @@ export default {
 
 <style lang="scss">
   .intro {
-    padding: 30px 0;
     background-color: rgb(245, 245, 245);
+    padding: 30px;
 
     h3 {
+      padding: 30px;
       font-weight: 700;
       color: orange;
+    }
+
+    h6 {
+      color: orange;
+
+      &:hover {
+        color: darken(orange, 7.5%);
+      }
+    }
+
+    img {
+      width: 100%;
+      height: auto;
+    }
+
+    figcaption {
+      margin: 30px;
+      text-align: center;
     }
   }
 </style>
